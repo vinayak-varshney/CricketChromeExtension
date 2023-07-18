7d704f94ddbd93d912e15c8e807a21e33dbf4d94
vinayak-varshney <93870082+vinayak-varshney@users.noreply.github.com>
2023-07-19 01:25:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+async function getData() {
+
+    return await fetch("https://api.cricapi.com/v1/currentMatches?apikey=7dce4353-d418-422e-9eb0-ac80996bb9ff&offset=0")
+        .then(data => data.json())
+        .then(data => {
+            if (data.status != "success")return;
+
+            const matchesList = data.data;
+
+            if(!matchesList)return [];
+            
+            
+            const relevantData = matchesList.map(match => `${match.name}, ${match.status}`);
+
+            console.log({relevantData});
+
+            document.getElementById("matches").innerHTML = relevantData.map(match => `<li>${match} </li>`).join('');
+
+            return relevantData;
+
+        })
+        .catch(e => console.log(e));
+}
+
+getData();
